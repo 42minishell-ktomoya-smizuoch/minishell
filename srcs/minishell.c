03e47ce4a001106bd385fabd08b9d297f56e9553
ktomoya <twbtomoya2@gmail.com>
2023-08-21 20:54:25 +0900

    add minishell.c

---
+++ srcs/minishell.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 18:50:09 by ktomoya           #+#    #+#             */
+/*   Updated: 2023/08/21 19:20:49 by ktomoya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (FAILURE);
+//	display_prompt();
+	launch_executable((const char *)argv[1]);
+	return (SUCCESS);
+}
