0bd2eb6bf309079bc0f94ab08218c98cf3608b21
ktomoya <twbtomoya2@gmail.com>
2023-08-21 20:54:49 +0900

    add minishell.h

---
+++ includes/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 14:47:22 by ktomoya           #+#    #+#             */
+/*   Updated: 2023/08/21 18:57:44 by ktomoya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+int 	display_prompt(void);
+void	launch_executable(const char *exe_path);
+
+#endif
