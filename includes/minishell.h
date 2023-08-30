3918c1b1acf7d78a404083659d2124acd693288e
ktomoya <twbtomoya2@gmail.com>
2023-08-30 11:39:35 +0900

    リストの構造体とトークンの構造体、トークンの種類を表す列挙体を追加

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:47:22 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/08/21 18:57:44 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/08/30 11:37:58 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,34 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-int 	display_prompt(void);
+# define SUCCESS 0
+
+typedef enum
+{
+	TYPE_GENERAL,
+	TYPE_PIPE,
+	TYPE_AMPERSAND,
+	TYPE_LEFT_PARENTHESIS,
+	TYPE_RIGHT_PARENTHESIS,
+	TYPE_REDIR,
+	TYPE_SPACE,
+	TYPE_TAB,
+	TYPE_NEWLINE
+}	e_type;
+
+typedef struct s_token
+{
+	char		*word;
+	e_type		type;
+}	t_token;
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	t_token			token;
+}	t_list;
+
+int		display_prompt(void);
 void	launch_executable(const char *exe_path);
 
 #endif
