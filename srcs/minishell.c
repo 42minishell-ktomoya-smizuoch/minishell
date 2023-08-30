e8ffadf093492c85f9cfd7000271af46f71a396b
ktomoya <twbtomoya2@gmail.com>
2023-08-30 20:44:08 +0900

    tokenize関数のテストコードを追加

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:50:09 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/08/21 19:20:49 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/08/30 17:09:42 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (FAILURE);
 //	display_prompt();
-	launch_executable((const char *)argv[1]);
+	// launch_executable((const char *)argv[1]);
 	return (SUCCESS);
 }
+
+// int main()
+// {
+//     const char *command = "echo \"Hello, world!\"";
+//     t_token_list *tokens = tokenize(command);
+//     t_token_list *current = tokens;
+
+//     while (current != NULL)
+//     {
+//         printf("Token: %s, Type: %d\n", current->token.word, current->token.type);
+//         current = current->next;
+//     }
+
+//     return 0;
+// }
