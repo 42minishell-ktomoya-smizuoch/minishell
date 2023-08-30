de3ce50383e61721de969f60c9cc8847b746f5af
ktomoya <twbtomoya2@gmail.com>
2023-08-30 20:43:28 +0900

    create_token関数とlstadd_back_tokenを追加

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,11 +6,35 @@
 /*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 16:37:58 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/08/30 12:55:31 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/08/30 20:38:30 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+bool	is_metachar(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == '|' || c == '&'
+		|| c == '(' || c == ')' || c == '<' || c == '>' || c == '\''
+		|| c == '\"');
+}
+
+char	*get_word(const char *str)
+{
+	char	*word;
+	size_t	len;
+
+	len = 0;
+	if (is_metachar(*str))
+		len++;
+	else
+	{
+		while (str[len] && !is_metachar(str[len]))
+			len++;
+	}
+	word = ft_substr(str, 0, len);
+	return (word);
+}
 
 enum e_type	get_token_type(const char *str)
 {
@@ -26,31 +50,61 @@ enum e_type	get_token_type(const char *str)
 		type = TYPE_RIGHT_PARENTHESIS;
 	else if (*str == '<' || *str == '>')
 		type = TYPE_REDIR;
-	else if (*str == ' ')
-		type = TYPE_SPACE;
-	else if (*str == '\t')
-		type = TYPE_TAB;
 	else if (*str == '\n')
 		type = TYPE_NEWLINE;
+	else if (*str == '\'')
+		type = TYPE_QUOTE;
+	else if (*str == '\"')
+		type = TYPE_DOUBLE_QUOTE;
 	else
 		type = TYPE_GENERAL;
 	return (type);
 }
 
-t_list	*tokenizer(const char *str)
+void	skip_spaces(const char **str)
 {
-	t_token_list	*head;
-	t_token_list	*tmp;
-	t_token			token;
+	while (**str == ' ' || **str == '\t')
+		(*str)++;
+}
 
+// t_token_list	*tokenize(const char *str)
+// {
+// 	t_token_list	*head;
+// 	t_token_list	*tmp;
+// 	t_token			token;
+
+// 	head = (t_token_list *)malloc(sizeof(t_token_list));
+// 	if (!head)
+// 		set_errno_and_exit("malloc error\n", ENOMEM);
+// 	while (*str)
+// 	{
+// 		skip_spaces(&str);
+// 		token.type = get_token_type(str);
+// 		token.word = get_word(str);
+// 		token.len = ft_strlen(token.word);
+// 		str += token.len;
+// 	}
+// 	return (head);
+// }
+
+t_token_list	*tokenize(const char *str)
+{
+	t_token			*new_token;
+	t_token_list	*head;
+
+	if (!str)
+		set_errno_and_exit("tokenize: str is NULL\n", EINVAL);
 	head = NULL;
 	while (*str)
 	{
-		token.word = ft_strdup(str);
-		token.type = get_token_type(str);
-		tmp = ft_lstnew(token);
-		ft_lstadd_back(&head, tmp);
-		str++;
+		new_token = create_token(str);
+		// if (!new)
+		// 	set_errno_and_exit("malloc error\n", ENOMEM);
+		// token.type = get_token_type(str);
+		// token.word = get_word(str);
+		// token.len = ft_strlen(token.word);
+		lstadd_back_token(&head, new_token);
+		str += new_token->len;
 	}
 	return (head);
 }
