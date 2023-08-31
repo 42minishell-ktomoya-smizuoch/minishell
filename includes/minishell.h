6e7ff8665c9d95627ce95e8b56198b26f456ee65
ktomoya <twbtomoya2@gmail.com>
2023-08-31 16:12:35 +0900

    token_list構造体を使わずにtoken構造体を使うようにした

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:47:22 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/08/31 14:49:53 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/08/31 15:51:22 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,20 +47,23 @@ typedef struct s_token
 	size_t			len;
 	enum e_type		type;
 	enum e_state	state;
+	struct s_token	*next;
 }	t_token;
 
-typedef struct s_token_list
-{
-	struct s_token_list	*next;
-	t_token				*token;
-}	t_token_list;
+// typedef struct s_token_list
+// {
+// 	struct s_token_list	*next;
+// 	t_token				*token;
+// }	t_token_list;
 
 int				display_prompt(void);
 void			launch_executable(const char *exe_path);
-t_token_list	*tokenize(const char *str);
+t_token			*tokenize(const char *str);
 void			set_errno_and_exit(const char *err_msg, int errno_num);
-t_token 		*create_token(const char *str);
-void			lstadd_back_token(t_token_list **lst, t_token *new);
+t_token			*create_token(const char *str);
+void			lstadd_back_token(t_token **lst, t_token *new);
+enum e_type		get_token_type(const char *str);
+char			*get_word(const char *str);
 
 
 
