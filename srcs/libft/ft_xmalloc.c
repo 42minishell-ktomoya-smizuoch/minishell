a11317e1a52a7d1fb475b5794c20b23e54e31900
ktomoya <twbtomoya2@gmail.com>
2023-08-31 16:11:55 +0900

    memoryの初期化問題を修正

--- srcs/libft/ft_xmalloc.c
+++ srcs/libft/ft_xmalloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_xmalloc.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 17:05:14 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/08/31 12:51:44 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/08/31 16:05:49 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	*ft_xmalloc(size_t size)
 {
 	void	*memory;
 
-	memory = NULL;
-	if (!(memory == malloc(size)))
+	memory = malloc(size);
+	if (!memory)
 	{
-		ft_putendl_fd("malloc error", 2);
+		ft_putendl_fd("malloc error", stderr);
 		exit(FAILURE);
 	}
 	return (memory);
