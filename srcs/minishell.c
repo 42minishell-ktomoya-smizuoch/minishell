16a85073002feea84091aba92f8ddac1bf82f22b
Shotaro Mizuochi <smizuoch@c1r22s7.42tokyo.jp>
2023-09-02 11:54:22 +0900

    Addition of readline and associated changes

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,11 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 18:50:09 by ktomoya           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/09/02 11:29:01 by smizuoch         ###   ########.fr       */
-=======
-/*   Updated: 2023/08/31 18:23:50 by ktomoya          ###   ########.fr       */
->>>>>>> e4e611e32ecaf2bdc70919108268fffcbaa3a95a
+/*   Updated: 2023/09/02 11:52:23 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +23,11 @@
 
 int	main()
 {
-<<<<<<< HEAD
 	t_token		*tokens;
 	t_token		*current;
 	const char	*command;
 
+	const char *command = "echo \"Hello,\"\' world!\'";
 	while (1)
 	{
 		command = readline("minishell$ ");
@@ -39,24 +35,10 @@ int	main()
 		current = tokens;
 		while (current != NULL)
 		{
-			printf("Token: %s, Type: %d\n", current->word, current->type);
+			printf("Token: %s, Type: %d, State: %d\n", current->word, current->type, current->state);
 			current = current->next;
 		}
 		free ((void *)command);
 	}
 	return (0);
 }
-=======
-    const char *command = "echo \"Hello,\"\' world!\'";
-    t_token     *tokens = tokenize(command);
-    t_token      *current = tokens;
-
-    while (current != NULL)
-    {
-        printf("Token: %s, Type: %d, State: %d\n", current->word, current->type, current->state);
-        current = current->next;
-    }
-
-    return 0;
-}
->>>>>>> e4e611e32ecaf2bdc70919108268fffcbaa3a95a
