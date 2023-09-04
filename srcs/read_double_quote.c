006b3883a0ab4a5eaa2f883551429eac433d39ff
ktomoya <twbtomoya2@gmail.com>
2023-09-04 12:05:44 +0900

    Tokenize function is partially complete

--- srcs/read_double_quote.c
+++ srcs/read_double_quote.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_double_quote.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 16:09:41 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/09/04 11:09:57 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/09/04 12:00:11 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 t_token	*read_double_quote(const char *str)
 {
 	t_token	*new_token;
-	
+
 	new_token = ft_xmalloc(sizeof(t_token));
 	new_token->type = TYPE_GENERAL;
-	new_token->len = 1;
+	new_token->len = ft_strlen("\"");
 	while (str[new_token->len] && str[new_token->len] != '\"')
-	{
-		printf("new_token->len = %zu\n", new_token->len);
-		printf("(*str)[new_token->len] = %c\n", str[new_token->len]);
 		new_token->len++;
-	}
+	if (str[new_token->len] == '\"')
+		new_token->len++;
 	new_token->next = NULL;
 	return (new_token);
 }
--- srcs/read_single_quote.c
+++ srcs/read_single_quote.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_single_quote.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 15:56:33 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/09/04 11:39:59 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/09/04 12:00:32 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,9 @@ t_token	*read_single_quote(const char *str)
 	new_token->type = TYPE_GENERAL;
 	new_token->len = 1;
 	while (str[new_token->len] && str[new_token->len] != '\'')
-	{
-		printf("(*str)[len] = %c\n", str[new_token->len]);
-		printf("len = %zu\n", new_token->len);
 		new_token->len++;
-	}
+	if (str[new_token->len] == '\'')
+		new_token->len++;
 	new_token->next = NULL;
 	return (new_token);
 }
