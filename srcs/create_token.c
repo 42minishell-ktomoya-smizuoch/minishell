e404f1af0c20d2af7996b05972b280ced3e738e4
tom <134240277+ktomoya1@users.noreply.github.com>
2023-09-05 13:36:50 +0900

    modified new->len

--- srcs/create_token.c
+++ srcs/create_token.c
@@ -3,25 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   create_token.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 17:17:52 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/09/04 20:07:12 by ktomoya          ###   ########.fr       */
+/*   Updated: 2023/09/05 12:35:44 by kudoutomoya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-t_token	*create_token(const char *str, enum e_state prev_state)
+t_token	*create_token(const char *str, size_t token_len)
 {
 	t_token	*new;
 
+	if (!token_len)
+		return (NULL);
 	new = (t_token *)ft_xmalloc(sizeof(t_token));
 	if (!new)
 		set_errno_and_exit("malloc error\n", ENOMEM);
 	new->type = get_token_type(str);
-	new->len = get_token_len(str, prev_state);
-	new->word = ft_substr(str, 0, new->len);
+	new->word = ft_substr(str, 0, token_len);
 	new->next = NULL;
 	return (new);
 }
