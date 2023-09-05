e279c623ecd692f93ff21f895e368cc187afa4fe
tom <134240277+ktomoya1@users.noreply.github.com>
2023-09-05 14:45:10 +0900

    update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 14:47:22 by ktomoya           #+#    #+#             */
-/*   Updated: 2023/09/05 14:14:25 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/09/05 14:42:18 by kudoutomoya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ typedef struct s_token
 {
 	char			*word;
 	enum e_type		type;
-	// size_t			len;
 	struct s_token	*next;
 }	t_token;
 
@@ -53,26 +52,13 @@ typedef struct s_token
 int				display_prompt(void);
 void			launch_executable(const char *exe_path);
 t_token			*lexer(const char *str);
-void			set_errno_and_exit(const char *err_msg, int errno_num);
-t_token			*create_token(const char *str, size_t token_len);
-void			lstadd_back_token(t_token **lst, t_token *new);
-// enum e_type		get_token_type(const char *str);
-// size_t			get_token_len(const char *str, enum e_state current);
 void			skip_spaces(const char **str);
+size_t			get_token_len(const char *str);
+t_token			*create_token(const char *str, size_t token_len);
+void			set_errno_and_exit(const char *err_msg, int errno_num);
+void			lstadd_back_token(t_token **lst, t_token *new);
 enum e_state	update_state(const char c, enum e_state state);
 bool			is_space_or_tab(char c);
 void			set_errno_and_exit(const char *str, int errnum);
-t_token			*read_pipe(const char c);
-t_token			*read_ampersand(const char c);
-t_token			*read_redirect(const char *str);
-t_token			*read_single_quote(const char *str);
-t_token			*read_double_quote(const char *str);
-t_token			*read_general(const char *str);
-// t_token			*read_quoted(const char *str);
-enum e_type		get_token_type(const char *str);
-size_t			get_double_quoted_size(const char *str);
-size_t			get_redirect_len(const char *str);
-size_t			get_token_len(const char *str);
-size_t			get_general_len(const char *str);
 
 #endif
