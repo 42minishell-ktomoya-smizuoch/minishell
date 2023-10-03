2c64430e95b05549bae867201035d4cf150ba519
tom <134240277+ktomoya1@users.noreply.github.com>
2023-10-03 18:31:40 +0900

    fix:access

--- srcs/playground/access.c
+++ srcs/playground/access.c
@@ -14,24 +14,72 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
-int	main(int argc, char *argv[])
+//int	main(int argc, char *argv[])
+//{
+//	if (argc != 2)
+//		exit(1);
+//	if (access(argv[1], R_OK) < 0)
+//	{
+//		printf("access error for %s\n", argv[1]);
+//		exit(1);
+//	}
+//	else
+//		printf("read access OK\n");
+//	if (open(argv[1], O_RDONLY) < 0)
+//	{
+//		printf("open error for %s\n", argv[1]);
+//		exit(1);
+//	}
+//	else
+//		printf("open for reading OK\n");
+//	exit(0);
+//}
+
+int main(int argc, char *argv[])
 {
 	if (argc != 2)
 		exit(1);
-	if (access(argv[1], R_OK) < 0)
-	{
-		printf("access error for %s\n", argv[1]);
-		exit(1);
-	}
+	if (access(argv[1], R_OK) == 0)
+		printf("%s は実行可能です。\n", argv[1]);
 	else
-		printf("read access OK\n");
-	if (open(argv[1], O_RDONLY) < 0)
 	{
-		printf("open error for %s\n", argv[1]);
-		exit(1);
+		if (errno == EACCES)
+			printf("%s は読み込み権限がありません。\n", argv[1]);
+		else if (errno == ENOENT)
+			printf("%s は存在しません。\n", argv[1]);
+		else
+			perror("access");
 	}
-	else
-		printf("open for reading OK\n");
-	exit(0);
+	return (0);
 }
+
+//int main() {
+//	const char *filename = "a.txt";
+//
+//	// ファイルを作成し、読み取り専用で開く
+//	int fd = open(filename, O_CREAT | O_RDONLY, 0444); // 0444 は読み取り専用の権限
+//
+//	if (fd == -1) {
+//		perror("open");
+//		return 1;
+//	}
+//
+//	// access関数を使用してファイルの読み込み権限をチェック
+//	if (access(filename, F_OK) == 0) {
+//		printf("%s は読み込み可能です。\n", filename);
+//	} else {
+//		if (errno == EACCES) {
+//			printf("%s は読み込み権限がありません。\n", filename);
+//		} else if (errno == ENOENT) {
+//			printf("%s は存在しません。\n", filename);
+//		} else {
+//			perror("access");
+//		}
+//	}
+//
+//	close(fd); // ファイルを閉じる
+//
+//	return 0;
+//}
