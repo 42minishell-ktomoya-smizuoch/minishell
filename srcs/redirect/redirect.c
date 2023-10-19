2b7cd1719cdf6165ffb85b10b1f2ea14fe255e68
ktomoya <twbtomoya2@gmail.com>
2023-10-19 19:02:58 +0900

    create redirect.c

--- srcs/redirect/redirect.c
+++ srcs/redirect/redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 09:53:58 by kudoutomoya       #+#    #+#             */
-/*   Updated: 2023/10/17 09:54:00 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/10/18 20:36:09 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,25 @@ void	putsyserr_exit(const char *syscall_name)
 /*
  * > filename
  */
-void	redirect_output(char *file)
+int	redirect_output(char *file)
 {
 	int	fd = open(file, O_WRONLY|O_CREAT|O_TRUNC, 0644);
 	if (fd == ERROR)
 		putsyserr_exit("open");
+	int tty = open("/dev/tty", O_RDWR);
+	if (tty == ERROR)
+		putsyserr_exit("open");
+	write(STDOUT_FILENO, "hello world\n", 12);
 	int result = dup2(fd, STDOUT_FILENO);
 	if (result == ERROR)
 		putsyserr_exit("dup2");
+	sleep(1);
+	write(STDOUT_FILENO, "after redirect\n", 12);
 	return (result);
+}
+
+int	main(void)
+{	
+	redirect_output("test.txt");
+	return (0);
 }
