2fb562109811dc39e3c566b7cdf3732075f557df
ktomoya <twbtomoya2@gmail.com>
2023-11-08 10:46:34 +0900

    add exit_status handle

--- srcs/execute/execute_error.c
+++ srcs/execute/execute_error.c
@@ -3,21 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   execute_error.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kudoutomoya <kudoutomoya@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ktomoya <ktomoya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 13:52:35 by kudoutomoya       #+#    #+#             */
-/*   Updated: 2023/10/14 14:02:56 by kudoutomoya      ###   ########.fr       */
+/*   Updated: 2023/11/08 10:28:09 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
 
-void	puterr_exit(const char *input, const char *msg)
+void	puterr(const char *input, const char *msg)
 {
 	ft_putstr_fd(input, STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putendl_fd(msg, STDERR_FILENO);
-	exit(FAILURE);
+}
+
+void	puterr_exit(const char *input, const char *msg, int exit_status)
+{
+	ft_putstr_fd(input, STDERR_FILENO);
+	ft_putstr_fd(": ", STDERR_FILENO);
+	ft_putendl_fd(msg, STDERR_FILENO);
+	exit(exit_status);
 }
 
 void	putsyserr_exit(const char *syscall_name)
