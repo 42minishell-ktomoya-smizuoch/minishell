cf45a68727525fb556698ce63d23dc7e3f9fbb7a
Shotaro Mizuochi <smizuoch@c1r22s8.42tokyo.jp>
2023-11-08 13:23:46 +0900

    add signal

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 15:16:43 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/11/07 16:22:38 by smizuoch         ###   ########.fr       */
+/*   Updated: 2023/11/08 13:12:41 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,29 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-void	handler_exex(int signum)
+void	handler_exec(int signum)
 {
 	if (signum == SIGINT)
 	{
 		write (STDIN_FILENO, "\n", 1);
+		g_signal = 1;
 	}
 	if (signum == SIGQUIT)
+	{
 		write (STDIN_FILENO, "Quit: 3\n", 8);
+		g_signal = 131;
+	}	
+		
 }
 
 void	handler_heardoc(int signum)
 {
 	(void)signum;
-	close (STDIN_FILENO);
+	if (signum == SIGINT)
+	{
+		close(0);
+		g_signal = 1;
+	}
 }
 
 void	handler_sigint(int signum)
@@ -41,20 +50,29 @@ void	handler_sigint(int signum)
 
 int	set_signal(int mode)
 {
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
 	if (mode == 0)
 	{
-		signal(SIGINT, handler_sigint);
-		signal(SIGQUIT, SIG_IGN);
+		sa.sa_handler = handler_sigint;
+		sa.sa_flags = 0;
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
 	}
 	else if (mode == 1)
 	{
-		signal(SIGINT, handler_exex);
-		signal(SIGQUIT, handler_exex);
+		sa.sa_handler = handler_exec;
+		sa.sa_flags = SA_SIGINFO;
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
 	}
 	else if (mode == 2)
 	{
-		signal(SIGINT, handler_heardoc);
-		signal(SIGQUIT, SIG_IGN);
+		sa.sa_handler = handler_heardoc;
+		sa.sa_flags = SA_SIGINFO;
+		sigaction(SIGINT, &sa, NULL);
+		sigaction(SIGQUIT, &sa, NULL);
 	}
 	return (0);
 }
